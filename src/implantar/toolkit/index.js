dd81efdfa53cbad546649b6ec1981b4f3d5ead74
md-555 <262286070md-555@users.noreply.github.com>
2026-02-18 15:39:31 -0300

    add denovo

--- src/implantar/toolkit/index.js
+++
@@ -1,7 +0,0 @@
-// Backwards-compatible entry points for consumers.
-// This package is ESM (package.json "type": "module").
-// Export minimal references so requiring/importing the package won't throw.
-export default {
-  eslintConfig: "./configs/eslint/base.js",
-  prettierConfig: "./configs/prettier/.prettierrc",
-};
